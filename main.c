3efea2727123e908ab6338a39e8e3a0d270791a5
aery <balkkaan@gmail.com>
2024-01-28 01:08:36 +0300

    first

---
+++ main.c
@@ -0,0 +1,12 @@
+void rush(int, int);
+
+int main(void) 
+{
+    rush(4, 4);
+    rush(5, 10);
+    rush(8, 8);
+    rush(9, 6);
+    rush(5, 12);
+    rush(3, 24);
+    return 0;
+}
---
+++ rush00.c
@@ -0,0 +1,77 @@
+void ft_putchar(char);
+
+void first_line(int x, int y)
+{
+    int i;
+
+    i = 0;
+    ft_putchar('/');
+    while(i < x - 2)
+    {
+        ft_putchar('*');
+        i++;
+    }
+    if(x > 1)
+    {
+        ft_putchar('\\');
+    }
+}
+
+void middle_line(int x, int y)
+{
+    int i;
+
+    i = 0;
+    ft_putchar('*');
+    while(i < x - 2)
+    {
+        ft_putchar(' ');
+        i++;
+    }
+    if(x > 1)
+    {
+        ft_putchar('*');
+    }
+}
+
+void last_line(int x, int y)
+{
+    int i;
+
+    i = 0;
+    ft_putchar('\\');
+    while(i < x - 2)
+    {
+        ft_putchar('*');
+        i++;
+    }
+    if(x > 1)
+    {
+        ft_putchar('/');
+    }
+}
+
+
+void rush (int x, int y)
+{
+    int middle_line_count;
+
+    middle_line_count = y - 2;
+    if(x > 0 && y > 0)
+    {
+        first_line(x, y);
+        while (middle_line_count > 0)
+        {
+            ft_putchar('\n');
+            middle_line(x, y);
+            middle_line_count = middle_line_count - 1;
+        }
+        ft_putchar('\n');
+        if(y > 1)
+            last_line(x, y);
+    }
+    ft_putchar('\n');
+    ft_putchar('\n');
+    ft_putchar('\n');
+
+}
